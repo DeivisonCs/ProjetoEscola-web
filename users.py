065282f0cd2a738fffd946ef50f194eb98092ca6
DeivisonSantos <deivisonddj3@gmail.com>
2023-12-17 23:25:45 -0300

    Adicionado verificação cpf e idade

--- users.py
+++ users.py
@@ -1,24 +1,44 @@
 from flask import Blueprint, render_template, request, redirect
 from database import db
 from models.tables import Students_Inf
+from datetime import datetime
 
 bp_Students = Blueprint("Students", __name__, template_folder="templates")
 
 @bp_Students.route('/create', methods=["GET", "POST"])
 def create():
     if request.method == 'GET':
-        return render_template("add_student.html")
+        return render_template("add_student.html", show_alert=False)
 
     if request.method == 'POST':
         name = request.form.get("name")
-        age = request.form.get("age")
+        birth_date = request.form.get("birth_date")
         cpf = request.form.get("cpf")
         email = request.form.get("email")
 
-        User = Students_Inf(name, age, cpf, email) 
-        db.session.add(User)
-        db.session.commit()
-        return "Dados Cadastrados"
+        valid = valid_cad(name, birth_date, cpf, email)
+
+        if valid == True:
+            age = valid_age(birth_date)
+
+            User = Students_Inf(name, age, cpf, email) 
+            db.session.add(User)
+            db.session.commit()
+            
+            return redirect("/")
+        
+        elif valid == 0:
+            return render_template('add_student.html', mensage='Preencha todos os campos!', show_alert=True)
+
+        elif valid == 1:
+            return render_template('add_student.html', mensage='Idade Inválida!', show_alert=True)
+        
+        elif valid == 2:
+            return render_template('add_student.html', mensage='CPF Inválido!', show_alert=True)
+
+        elif valid == 3:
+            return render_template('add_student.html', mensage='CPF já Cadastrado!', show_alert=True)
+        
 
 
 @bp_Students.route('/list')
@@ -36,7 +56,7 @@ def update_student(id):
     
     if request.method == 'POST':
         User.name = request.form.get("name")
-        User.age = request.form.get("age")
+        User.birth_date = request.form.get("birth_date")
         User.cpf = request.form.get("cpf")
         User.email = request.form.get("email")
 
@@ -53,4 +73,56 @@ def remove_student(id):
 
     db.session.delete(User)
     db.session.commit()
-    return redirect("list_students.html")
+    return redirect("/User/list")
+
+
+
+# ------------------------ Validações -----------------------
+
+def valid_cad(name, birth_date, cpf, email):
+
+    if name=="" or birth_date=="" or cpf=="" or email=="":
+        return 0
+
+    age = valid_age(birth_date)
+
+    if age <= 0:
+        return 1
+    
+    cpf_valid = valid_cpf(cpf)
+
+    if cpf_valid == "CPF inválido":
+        return 2
+    
+    if cpf_valid == "CPF ja cadastrado":
+        return 3
+    
+    return True
+    
+
+
+def valid_cpf(cpf):
+    only_Num_CPF = ''.join(filter(str.isdigit, cpf))
+    
+    if len(only_Num_CPF) != 11:
+        return "CPF inválido"
+
+    else:
+        Users = Students_Inf.query.all()
+        for user in Users:
+            no_Num_CPF = ''.join(filter(str.isdigit, user.cpf))
+
+            if no_Num_CPF == only_Num_CPF:
+                return "CPF ja cadastrado"
+
+    return True
+
+def valid_age(birth_date):
+    age = datetime.strptime(birth_date, "%Y-%m-%d")
+    date_atual = datetime.now()
+    diff = date_atual - age
+    age = diff.days // 365
+
+    return age
+
+# 000.000.000-00
