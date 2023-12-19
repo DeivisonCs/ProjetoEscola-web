c5e1b821ed82cbce0deb7bcf2cd27b508bcce6b6
DeivisonSantos <deivisonddj3@gmail.com>
2023-12-18 22:56:37 -0300

    Verificação de atualização de usuários

--- users.py
+++ users.py
@@ -39,6 +39,9 @@ def create():
         elif valid == "CPF ja cadastrado":
             return render_template('add_student.html', mensage='CPF já Cadastrado!', show_alert=True)
         
+        elif valid == "Email em Uso":
+            return render_template('add_student.html', mensage='Email em Uso!', show_alert=True)
+        
 
 
 @bp_Students.route('/list')
@@ -50,21 +53,46 @@ def list_students():
 @bp_Students.route('/update/<int:id>', methods=['GET', 'POST'])
 def update_student(id):
     User = Students_Inf.query.get(id)
+    Users = Students_Inf.query.all()
 
     if request.method == 'GET':
         return render_template('update_student.html', User=User )
     
     if request.method == 'POST':
-        User.name = request.form.get("name")
-        User.birth_date = request.form.get("birth_date")
-        User.cpf = request.form.get("cpf")
-        User.email = request.form.get("email")
+        name = request.form.get("name")
+        age = request.form.get("birth_date")
+        cpf = request.form.get("cpf")
+        email = request.form.get("email")
 
-        db.session.add(User)
-        db.session.commit()
+        valid = valid_updt(User.id, name, age, cpf, email)
+        
+        if valid == True:
+            User.name = name
+            User.age = age
+            User.cpf = cpf
+            User.email = email
 
-        User = Students_Inf.query.all()
-        return render_template("list_students.html", Students_all=User)
+            db.session.add(User)
+            db.session.commit()
+            
+            # User = Students_Inf.query.all()
+            # return render_template("list_students.html", Students_all=User)
+            return redirect("/User/list")
+        
+        elif valid == 0:
+            return render_template('update_student.html', User=User, mensage='Preencha todos os campos!', show_alert=True)
+        
+        elif valid == "Idade Inválida!":
+            return render_template('update_student.html', User=User, mensage='Idade Inválida!', show_alert=True)
+        
+        elif valid == "CPF inválido":
+            return render_template('update_student.html', User=User, mensage='CPF Inválido!', show_alert=True)
+
+        elif valid == "CPF ja cadastrado":
+            return render_template('update_student.html', User=User, mensage='CPF já Cadastrado!', show_alert=True)
+        
+        elif valid == "Email em Uso":
+            return render_template('update_student.html', User=User, mensage='Email em Uso!', show_alert=True)
     
 
 @bp_Students.route('/remove/<int:id>', methods=['GET'])
@@ -79,6 +107,7 @@ def remove_student(id):
 
 # ------------------------ Validações -----------------------
 
+# Validação do Cadastro de Novo Aluno
 def valid_cad(name, birth_date, cpf, email):
 
     if name=="" or birth_date=="" or cpf=="" or email=="":
@@ -97,8 +126,42 @@ def valid_cad(name, birth_date, cpf, email):
     if cpf_valid == "CPF ja cadastrado":
         return "CPF ja cadastrado"
     
+    Users = Students_Inf.query.all()
+
+    for user in Users:
+        if user.email == email:
+            return "Email em Uso"
+
     return True
+
+
+def valid_updt(id, name, age, cpf, email):
+
+    if name=="" or age=="" or cpf=="" or email=="":
+        return 0
+
+    if int(age) <= 0:
+        return "Idade Inválida!"
     
+    only_Num_CPF = ''.join(filter(str.isdigit, cpf))
+    
+    if len(only_Num_CPF) != 11:
+        return "CPF inválido"
+
+    else:
+        Users = Students_Inf.query.all()
+
+        for user in Users:
+            no_Num_CPF = ''.join(filter(str.isdigit, user.cpf))
+
+            if no_Num_CPF == only_Num_CPF and user.id != id:
+                return "CPF ja cadastrado"
+            
+            elif user.email == email and user.id != id:
+                return "Email em Uso"
+    
+    
+    return True
 
 
 def valid_cpf(cpf):
